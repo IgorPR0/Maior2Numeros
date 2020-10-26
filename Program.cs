31ce7d652fd744c058569ddc241705655bc058c8
IgorPR0 <igortecnologia2020@gmail.com>
2020-10-26 15:24:25 -0300

    Completed activity

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace maior2ums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double comun,maior = 0;
+        
+            Console.Write("First number: ");
+            comun = Convert.ToDouble(Console.ReadLine());
+
+            if (comun > maior)
+            {
+                maior = comun;
+            }
+
+            Console.Write("Second number: ");
+            comun = Convert.ToDouble(Console.ReadLine());
+
+            if (comun > maior)
+            {             
+                maior = comun;
+            }
+            
+            Console.WriteLine($"The largest number is: {maior}");
+            
+        }
+    }
+}
